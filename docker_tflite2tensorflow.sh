11d2dcec479d3d34b428cf70f037712feb212cbb
Kazuya Yamamoto <kazuya_yamamoto@mail.toyota.co.jp>
2023-01-27 18:08:42 +0900

    Change referenced files to submodule

--- docker_tflite2tensorflow.sh
+++
@@ -1,8 +0,0 @@
-# Used to convert original Mediapipe tflite models to Openvino IR and MyriadX blob
-# https://github.com/PINTO0309/tflite2tensorflow
-#
-# First run: docker pull ghcr.io/pinto0309/tflite2tensorflow:latest
-docker run -it --rm \
-    -v `pwd`:/home/user/workdir \
-    ghcr.io/pinto0309/tflite2tensorflow:latest
-
